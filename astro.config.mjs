fd63758d0c3bdd11a09ef8cb3ad99bbbe493330d
Ranatrat <ranatrat1@gmail.com>
2026-03-31 19:38:39 +0000

    Astro config ve base yolu güncellendi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  
+  // Sitenin internetteki ana adresi (Senin GitHub kullanıcı adınla)
+  site: 'https://Ranatrat.github.io',
+  
+  // Repository (depo) adın. Başına ve sonuna / koymalısın.
+  base: '/yavuzyekvucut.me',
+  
   integrations: [tailwind()]
 });
